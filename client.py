b27141c17cd1eff2868ffb3f8689a741c9b33b01
Prabesh Khatri <prkha2082@oslomet.no>
2025-05-13 23:21:30 +0200

    Handshake established with the help of packets

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from socket import *
+from packets import Packets
 
 class Client:
 
@@ -8,23 +9,35 @@ class Client:
         serverSocket = (serverIP, serverPort)
         print("Connection Establishment Phase: \n")
         
-        #SYN packet
-        syn = "SYN packet is sent"
-        clientSocket.sendto(syn.encode(), serverSocket)
-        print(syn)
+        # Send a SYN packet to the server
+        syn = Packets.create_packet(0, 0, 8, 0, b'')
+        clientSocket.sendto(syn, serverSocket)
 
-        while True:
-            response, serverAddress = clientSocket.recvfrom(1024)
-            response = response.decode()
-            if (response == "SYN-ACK packet is sent"):
-                print("SYN-ACK packet is received")
-                # Send a response back to the server
-                clientSocket.sendto("ACK packet is sent".encode(), serverSocket)
-                print("ACK packet is sent")
+        try:
+            while True:
+                # Receive data from the server
+                packet_header, serverAddress = clientSocket.recvfrom(1024)
 
-            elif (response == "Connection established"):
-                print("Connection established\n")
-                break
+                # Unpack the received packet
+                seq, ack, flags, win = Packets.parse_header(packet_header)
+
+                # Check the flags to determine the type of packet received
+                syn, ack, fin = Packets.parse_flags(flags)
+
+                if (syn == 8 and ack == 4):
+                    print("SYN-ACK packet is received")
+                    # Send a response back to the server
+                    ack = Packets.create_packet(0, 0, 4, 0, b'')
+                    clientSocket.sendto(ack, serverSocket)
+                    print("ACK packet is sent")
+                    print("Connection established\n")
+                    break
+            # File Transfer Phase
+            print("Data Transfer:\n")
+            
+
+        except Exception as KeyboardInterrupt:
+            print("Connection Terminated")
 
         clientSocket.close()
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from socket import *
+from packets import Packets
 import _thread as thread
 
 class Server:
@@ -7,8 +8,27 @@ class Server:
         """
         Handles a single client request.
         """
-        response = data.decode()
-        if response == "SYN packet is sent":
+        # Unpack the received packet
+        packet_header = data[:8]
+        seq, ack, flags, win = Packets.parse_header(packet_header)
+        
+		# Check the flags to determine the type of packet received
+        syn, ack, fin = Packets.parse_flags(flags)
+        
+        if (syn == 8):
+            print("SYN packet is received")
+            # Send a response back to the client
+            syn_ack = Packets.create_packet(0, 0, 12, 0, b'')
+            serverSocket.sendto(syn_ack, clientAddress)
+            print("SYN-ACK packet is sent")
+        
+        elif (ack == 4):
+            print("ACK packet is received")
+            print("Connection established\n")
+            
+
+            
+        """if response == "SYN packet is sent":
             print("SYN packet is received")
             # Send a response back to the client
             serverSocket.sendto("SYN-ACK packet is sent".encode(), clientAddress)
@@ -18,12 +38,11 @@ class Server:
             # Send a response back to the client
             serverSocket.sendto("Connection established".encode(), clientAddress)
             print("Connection established")
-
+		"""
     @staticmethod
     def main(serverIP, serverPort):
         serverSocket = socket(AF_INET, SOCK_DGRAM)  # Create a UDP socket
         serverSocket.bind((serverIP, serverPort))  # Bind to the specified IP and port
-        print(f"UDP server is ready to receive on port {serverPort}")
 
         while True:
             # Receive data and client address
