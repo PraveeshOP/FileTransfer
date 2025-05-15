89f886e2828fdd6ca2274580dd19203b92c048bd
Prabesh Khatri <prkha2082@oslomet.no>
2025-05-15 20:57:14 +0200

    argument added in application.py

--- application.py
+++ application.py
@@ -11,19 +11,22 @@ parser.add_argument('-c', '--client', action='store_true', help="Run as client")
 # Optional filename for the client
 parser.add_argument('-f', '--filename', metavar='', type=str, help="Please provide the filename")
 
+# Optional window size for the client
+parser.add_argument('-w', '--window', metavar='', type=int, help="Please provide the window size")
+
 # Required arguments for IP and port
 parser.add_argument('-i', '--IP', metavar='', type=str, required=True, help="Please provide the IP address of the server")
 parser.add_argument('-p', '--port', metavar='', type=int, required=True, help="Please provide the port number of the server")
 
-# Required window size for the client
-parser.add_argument('-w', '--window', metavar='', type=int, help="Please provide the window size")
+#Optional discard value for the server
+parser.add_argument('-d', '--discard', metavar='', type=int, help="Please provide the discard number for the server")
 
 args = parser.parse_args()
 
 def main():
     if args.server:
         # If -s is provided, run the Server's main method
-        Server.main(args.IP, args.port)
+        Server.main(args.IP, args.port, args.discard)
     elif args.client:
         # If -c is provided, run the Client's main method
         # Check if filename is provided, if it is not provided then give error message and return
